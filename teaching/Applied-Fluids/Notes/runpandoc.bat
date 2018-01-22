e102b1dce0397e8f10daf466d0d902968a81dc16
kdusling <kdusling@gmail.com>
2018-01-22 07:27:09 -0500

    'update'

---
+++ teaching/Applied-Fluids/Notes/runpandoc.bat
@@ -0,0 +1,8 @@
+
+
+
+pandoc -s -H header.text -A footer.text -c style.css units.text --mathjax -o units.html
+
+
+pandoc -s -H header.text -A footer.text -c style.css MinorLosses.text --mathjax -o MinorLosses.html
+pandoc -s -H header.text -A footer.text -c style.css buoyancy.text --mathjax -o buoyancy.html
