5367b2f9e0449cf2fccf7e389db8aad49c770c62
kdusling <kdusling@gmail.com>
2018-02-15 08:45:06 -0500

    'update'

---
+++ teaching/Applied-Fluids/Notes/runpandoc.sh
@@ -0,0 +1,4 @@
+
+pandoc -s -H header.text --toc -A footer.text -c style.css units.text --mathjax -o units.html
+pandoc -s -H header.text --toc -A footer.text -c style.css buoyancy.text --mathjax -o buoyancy.html
+pandoc -s -H header.text --toc -A footer.text -c style.css MinorLosses.text --mathjax -o MinorLosses.html
