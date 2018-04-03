f0384cf255e41fd630d9f7b03a2025c327c50ade
kdusling <kdusling@gmail.com>
2018-04-02 21:42:31 -0400

    'update'

---
+++ teaching/Applied-Fluids/Liquids.js
@@ -0,0 +1,9 @@
+
+var liquids=[
+{id:"Acetone", display:"Acetone", sg:0.787, vis:0.316, st:23.1},
+{id:"Benzene", display:"Benzene", sg:0.876, vis:0.601, st:28.18},
+{id:"Glycerine", display:"Glycerine", sg:1.263, vis:950, st:63.0},
+{id:"Octane", display:"Octane", sg:0.701, vis:0.51, st:21.14},
+{id:"Propane", display:"Propane", sg:0.495, vis:0.11, st:6.6},
+{id:"Water", display:"Water", sg:0.997, vis:0.891, st:71.97},
+];
