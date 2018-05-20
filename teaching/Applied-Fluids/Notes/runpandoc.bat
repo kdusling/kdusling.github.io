6131b6d1c9f9167ec75f98ed86a92c9cfd7b9b62
kdusling <kdusling@gmail.com>
2018-05-20 15:20:19 -0400

    update

--- teaching/Applied-Fluids/Notes/runpandoc.bat
+++
@@ -1,4 +0,0 @@
-
-pandoc -s -H header.text --toc -A footer.text -c style.css units.text --mathjax -o units.html
-pandoc -s -H header.text --toc -A footer.text -c style.css FrictionLosses.text --mathjax --filter pandoc-eqnos -o FrictionLosses.html
-pandoc -s -H header.text --toc -A footer.text -c style.css buoyancy.text --mathjax -o buoyancy.html
--- teaching/Applied-Fluids/Notes/runpandoc.sh
+++
@@ -1,4 +0,0 @@
-
-pandoc -s -H header.text --toc -A footer.text -c style.css units.text --mathjax -o units.html
-pandoc -s -H header.text --toc -A footer.text -c style.css buoyancy.text --mathjax -o buoyancy.html
-pandoc -s -H header.text --toc -A footer.text -c style.css MinorLosses.text --mathjax -o MinorLosses.html
