fb10999a0c1a12d53fe8c9d279056eff3259904d
kdusling <kdusling@gmail.com>
2018-07-18 13:54:55 -0400

    hw1

---
+++ teaching/Applied-Fluids/Homework/Units.md
@@ -0,0 +1,9 @@
+---
+layout: course
+title: Dimensions & Units
+---
+
+# Dimensions & Units
+{:.no_toc}
+
+![](/png/unit1-001.png)
---
+++ teaching/Applied-Fluids/Homework/compile.bat
@@ -0,0 +1,2 @@
+pdflatex unit1.tex
+"C:\Program Files\gs\gs9.23\bin\gswin64c.exe" -sDEVICE=png16m -dTextAlphaBits=4 -o png/unit1-%%03d.png -r1200 unit1.pdf
