4a63bb592383cfb18094fa5ae9bf526669480705
kdusling <kdusling@gmail.com>
2018-07-18 14:48:42 -0400

    hw down

--- teaching/Applied-Fluids/Homework/compile.bat
+++ teaching/Applied-Fluids/Homework/compile.bat
@@ -1,2 +1,2 @@
 pdflatex unit1.tex
-"C:\Program Files\gs\gs9.23\bin\gswin64c.exe" -sDEVICE=png16m -dTextAlphaBits=4 -dDownScaleFactor=2 -o png/unit1-%%03d.png -r600 unit1.pdf
+"C:\Program Files\gs\gs9.23\bin\gswin64c.exe" -sDEVICE=png16m -dTextAlphaBits=4 -dDownScaleFactor=3 -o png/unit1-%%03d.png -r600 unit1.pdf
