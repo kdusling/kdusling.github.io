83c716c5a30b95266c1b775850db5d92c5838eed
kdusling <kdusling@gmail.com>
2018-07-18 14:58:36 -0400

    hw dc

--- teaching/Applied-Fluids/Homework/compile.bat
+++ teaching/Applied-Fluids/Homework/compile.bat
@@ -1,2 +1,2 @@
 pdflatex unit1.tex
-"C:\Program Files\gs\gs9.23\bin\gswin64c.exe" -sDEVICE=png16m -dTextAlphaBits=4 -dDownScaleFactor=5 -o png/unit1-%%03d.png -r600 unit1.pdf
+"C:\Program Files\gs\gs9.23\bin\gswin64c.exe" -sDEVICE=png16m -dTextAlphaBits=4 -dDownScaleFactor=4 -o png/unit1-%%03d.png -r1200 unit1.pdf
