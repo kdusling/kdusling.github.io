b9c95ef5bf4aad551232977ced582032f966ea18
kdusling <kdusling@gmail.com>
2019-08-28 13:24:04 -0400

    liquids

--- teaching/Applied-Fluids/Liquids.js
+++ teaching/Applied-Fluids/Liquids.js
@@ -5,7 +5,7 @@ var liquids=[
 {id:"Glycerine", display:"Glycerine",    sg:1.263, vis:950,   st:63.0,  K:4.35, vp:0},
 {id:"Hexane", display:"Hexane",          sg:0.657, vis:0.297, st:18.0,  K:0.60, vp:17.6},
 {id:"Kerosene", display:"Kerosene",      sg:0.823, vis:1.64,  st:0,     K:1.3,  vp:0.7},
-{id:"Mercury", display:"Mercury",        sg:13.54, vis:1.53,  st:485,   K:28.5,  vp:0.00023},
+{id:"Mercury", display:"Mercury",        sg:13.54, vis:1.53,  st:485,   K:28.5, vp:0.00023},
 {id:"Octane", display:"Octane",          sg:0.701, vis:0.51,  st:21.14, K:0,    vp:1.9},
 {id:"Propane", display:"Propane",        sg:0.495, vis:0.11,  st:6.6,   K:0,    vp:0},
 {id:"SeaWater", display:"Sea Water",     sg:1.030, vis:0.103, st:0,     K:2.34, vp:2.4},
