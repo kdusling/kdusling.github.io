f7401a67246fcdf3d6f9e3b96979f0a66e892082
kdusling <kdusling@gmail.com>
2022-11-30 13:13:23 -0500

    macros

--- teaching/Applied-Fluids/examples/local.js
+++ teaching/Applied-Fluids/examples/local.js
@@ -1,10 +1,46 @@
 MathJax.Hub.Config({
   TeX: {
     Macros: {
-      RR: "{\\bf R}",
-      bold: ["{\\bf #1}",1]
-    }
+        s: '{\\mathrm{s}}',
+        min: '{\\mathrm{min}}',
+        hr: '{\\mathrm{hr}}',
+        kg: '{\\mathrm{kg}}',
+        slug: '{\\mathrm{slugs}}',
+        N: '{\\mathrm{N}}',
+        kN: '{\\mathrm{kN}}',
+        inch: '{\\mathrm{in}}',
+        ft: '{\\mathrm{ft}}',
+        m: '{\\mathrm{m}}',
+        mm: '{\\mathrm{mm}}',
+        km: '{\\mathrm{km}}',
+        mi: '{\\mathrm{mi}}',
+        mph: '{\\mathrm{mph}}',
+        cm: '{\\mathrm{cm}}',
+        lb: '{\\mathrm{lb}}',
+        lbm: '{\\mathrm{lbm}}',
+        lbf: '{\\mathrm{lbf}}',
+        Pa: '{\\mathrm{Pa}}',
+        kPa: '{\\mathrm{kPa}}',
+        MPa: '{\\mathrm{MPa}}',
+        psi: '{\\mathrm{psi}}',
+        C: '{^\\circ\\mathrm{C}}',
+        F: '{^\\circ\\mathrm{F}}',
+        R: '{\\mathrm{R}}',
+        K: '{\\mathrm{K}}',
+        gal: '{\\mathrm{gal}}',
+        L: '{\\mathrm{L}}',
+        cfm: '{\\mathrm{cfm}}',
+        fps: '{\\mathrm{fps}}',
+        fpm: '{\\mathrm{fpm}}',
+        gpm: '{\\mathrm{gpm}}',
+        sg: '{\\mathrm{sg}}',
+        Re: '{\\mathrm{Re}}',
+        WP: '{\\mathrm{WP}}',
+        inWC: '{\\mathrm{inH}_2\\mathrm{O}}',
+        hp: '{\\mathrm{hp}}',
+        lbftovs: '{\\mathrm{lb}\\cdot\\mathrm{ft}/\\mathrm{s}}',
+      }
   }
 });
 
-MathJax.Ajax.loadComplete("http://myserver.com/MathJax/config/local/local.js");
+MathJax.Ajax.loadComplete("https://kdusling.github.io/teaching/Applied-Fluids/examples/local.js");
